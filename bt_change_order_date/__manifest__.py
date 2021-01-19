e680d64427f040a995fb09eba1fbac20984066b0
binu <binufrancis@broadtech-innovations.com>
2021-01-19 17:12:18 +0530

    new module bt_change_order_date

---
+++ bt_change_order_date/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BroadTech Change Sale Order Date",
+
+    'summary': """
+    BroadTech Change Sale Order Date
+    """,
+
+    'description': """
+    BroadTech Change Sale Order Date
+    """,
+
+    'author': "Broadtech IT solutions pvt lmt",
+    'website': "https://www.broadtech-innovations.com",
+
+    'category': '',
+    'version': '0.1',
+
+    'depends': [
+        'sale',
+    ],
+
+    'data': [
+        'wizard/change_sale_order_date_view.xml',
+        'views/change_order_date_view.xml',
+    ],
+
+}

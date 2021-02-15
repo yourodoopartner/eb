17dbf8ba61ceb05eb86fbe73199cb1ebe5325f66
philpinsky <62351785+yourodoopartner@users.noreply.github.com>
2021-02-15 21:14:14 +0530

    changed name into Everblast / Explojeux

--- bt_email_from_name_static/models/ir_mail_server.py
+++ bt_email_from_name_static/models/ir_mail_server.py
@@ -15,7 +15,7 @@ class IrMailServer(models.Model):
                     attachments=None, message_id=None, references=None, object_id=False, subtype='plain', headers=None,
                     body_alternative=None, subtype_alternative='plain'):
         
-        email_from = 'Everblast <admin@everblastto.com>'
+        email_from = 'Everblast / Explojeux <admin@everblastto.com>'
         
         return super(IrMailServer, self).build_email(email_from, email_to, subject, body, email_cc=email_cc, email_bcc=email_bcc, reply_to=reply_to,
                 attachments=attachments, message_id=message_id, references=references, object_id=object_id, subtype=subtype, headers=headers,

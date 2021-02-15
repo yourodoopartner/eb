ff60cc0d6710dd0278232036b9d0b6cbf49094bc
Broadtech <fahad@broadtech-innovations.com>
2021-02-15 14:05:49 +0530

    new module to set email from name Everblast

---
+++ bt_email_from_name_static/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Email From Address Name Static',
+    'version': '13.0.1.0',
+    'category': 'Mail',
+    'description': u"""
+This module is to set static email from address name on send mail.
+""",
+    'author' : 'BroadTech IT Solutions Pvt Ltd',
+    'website' : 'http://www.broadtech-innovations.com',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'application': False,
+    'license': 'OPL-1',
+}

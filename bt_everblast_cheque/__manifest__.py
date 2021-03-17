a556e7d34cc6be3b63e680084416678c9a273e75
binu <binufrancis@broadtech-innovations.com>
2021-03-17 16:25:56 +0530

    bt_everblast_cheque

---
+++ bt_everblast_cheque/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BroadTech Everblast Cheque",
+
+    'summary': """
+    BroadTech Everblast Cheque
+    """,
+
+    'description': """
+    BroadTech Everblast Cheque
+    """,
+
+    'author': "Broadtech IT solutions pvt lmt",
+    'website': "https://www.broadtech-innovations.com",
+
+    'category': '',
+    'version': '0.1',
+
+    'depends': [
+        'l10n_ca_check_printing',
+    ],
+
+    'data': [
+        'report/print_check.xml',
+        'report/print_check_top.xml',
+
+    ],
+
+}
